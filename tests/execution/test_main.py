1e1b9688e5ca413ef2a66c58efaffdba8bc8c219
Ezequiel Valencia <ezq.valencia@gmail.com>
2026-02-26 11:12:38 -0500

    Fix Test Main

--- tests/execution/test_main.py
+++ tests/execution/test_main.py
@@ -4,17 +4,18 @@ import tempfile
 from pathlib import Path
 from typing import Any
 
-from process_bigraph import Composite, ProcessTypes
+from bigraph_schema import Core
+from process_bigraph import Composite
 
 from pbest.main import run_experiment
 from pbest.utils.input_types import ExecutionProgramArguments
 
 
 # Write a pbg that is used for comparison between Copasi and Tellurium, run it, and then check results
-def test_run_experiment(comparison_document: dict[Any, Any], fully_registered_core: ProcessTypes) -> None:
+def test_run_experiment(comparison_document: dict[Any, Any], fully_registered_core: Core) -> None:
     with tempfile.TemporaryDirectory() as tmpdir:
-        comp = Composite(config=comparison_document, core=fully_registered_core)
-        comp.save(filename="input.pbg", outdir=tmpdir)
+        with open(f"{tmpdir}/input.pbg", "w") as file:
+            file.write(json.dumps(comparison_document))
 
         os.mkdir(f"{tmpdir}/output")
         program_arguments = ExecutionProgramArguments(
