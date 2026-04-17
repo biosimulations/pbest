cade96184ec8da3c5e0040e6bad20ece4a81dad2
Ezequiel Valencia <ezq.valencia@gmail.com>
2026-04-17 11:52:37 -0400

    Template In PBest Tag

--- pbest/containerization/container_constructor.py
+++ pbest/containerization/container_constructor.py
@@ -51,7 +51,9 @@ def get_experiment_deps() -> ExperimentPrimaryDependencies:
 
 
 def formulate_dockerfile_for_necessary_env(
-    program_arguments: ContainerizationProgramArguments, experiment_deps: ExperimentPrimaryDependencies
+    program_arguments: ContainerizationProgramArguments,
+    experiment_deps: ExperimentPrimaryDependencies,
+    pbest_tag: str = "0.5.2",
 ) -> ContainerizationFileRepr:
     # pb_document_str: str
     deps_install_command: str = ""
@@ -81,8 +83,7 @@ def formulate_dockerfile_for_necessary_env(
     with open(__file__.rsplit(os.sep, maxsplit=1)[0] + f"{os.sep}generic_container.jinja") as f:
         template = Template(f.read())
         templated_container = template.render(
-            dependencies_to_install=deps_install_command,
-            micromamba_env_path=micromamba_env_path,
+            dependencies_to_install=deps_install_command, micromamba_env_path=micromamba_env_path, pbest_tag=pbest_tag
         )
 
     return ContainerizationFileRepr(representation=templated_container)
